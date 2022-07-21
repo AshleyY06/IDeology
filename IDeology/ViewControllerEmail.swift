a8dbfb4a2c7ca855f374f3c629b6731e99b5d2e6
tkuchi <92394716+tkuchi@users.noreply.github.com>
2022-07-20 20:00:01 -0400

    Made the inbox screen better

---
+++ IDeology/ViewControllerEmail.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerEmail.swift
+//  IDeology
+//
+//  Created by Tanishqa Kuchi on 7/20/22.
+//
+
+import UIKit
+
+class ViewControllerEmail: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
