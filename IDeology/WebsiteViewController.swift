469ed3e891e24b324fa89bcdf9cbe4f763156fd1
tkuchi <92394716+tkuchi@users.noreply.github.com>
2022-07-22 11:06:00 -0400

    added website

---
+++ IDeology/WebsiteViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WebsiteViewController.swift
+//  IDeology
+//
+//  Created by Tanishqa Kuchi on 7/22/22.
+//
+
+import UIKit
+import WebKit
+
+class WebsiteViewController: UIViewController {
+    
+    
+    @IBOutlet weak var infoLinks: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let googleURL = URL(string: "https://www.occ.gov/topics/consumers-and-communities/consumer-protection/fraud-resources/phishing-attack-prevention.html")
+        infoLinks.load(URLRequest(url:googleURL!))
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
